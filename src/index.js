b6febc580ad77050356a52541084dce25e75a4d1
Yugo Ogura <jigenbakdaaan@gmail.com>
2019-08-21 21:02:25 +0900

    カードを重ねる

--- src/index.js
+++ src/index.js
@@ -5,15 +5,18 @@ import "./styles.css";
 import { Card } from "./card";
 
 function App() {
-  const [user, setUser] = React.useState({
+  const user = {
     name: "Yugo Spice Ogura",
     age: 23,
     image:
       "https://images.unsplash.com/photo-1563170423-741125661ae8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80"
-  });
+  };
   return (
     <div className="App">
-      <Card name={user.name} age={user.age} image={user.image} />
+      <div className="card-container">
+        <Card name={user.name} age={user.age} image={user.image} />
+        <Card name={user.name} age={user.age} image={user.image} />
+      </div>
       <div className="buttons">
         <button className="button-nope">Nope</button>
         <button className="button-like">Like</button>
