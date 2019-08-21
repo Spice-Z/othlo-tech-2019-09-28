1e27d7b46e930b68f32356e3568a881adf90ea51
Yugo Ogura <jigenbakdaaan@gmail.com>
2019-08-21 21:06:56 +0900

    重ねたカードの中身を書き換える

--- src/index.js
+++ src/index.js
@@ -5,17 +5,25 @@ import "./styles.css";
 import { Card } from "./card";
 
 function App() {
-  const user = {
-    name: "Yugo Spice Ogura",
-    age: 23,
-    image:
-      "https://images.unsplash.com/photo-1563170423-741125661ae8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80"
-  };
+  const users = [
+    {
+      name: "Yugo Spice Ogura",
+      age: 23,
+      image:
+        "https://images.unsplash.com/photo-1563170423-741125661ae8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80"
+    },
+    {
+      name: "Yugo BOXP Ogura",
+      age: 26,
+      image:
+        "https://images.unsplash.com/photo-1565385320868-2d6aa499b93d?ixlib=rb-1.2.1&auto=format&fit=crop&w=595&q=80"
+    }
+  ];
   return (
     <div className="App">
       <div className="card-container">
-        <Card name={user.name} age={user.age} image={user.image} />
-        <Card name={user.name} age={user.age} image={user.image} />
+        <Card name={users[0].name} age={users[0].age} image={users[0].image} />
+        <Card name={users[1].name} age={users[1].age} image={users[1].image} />
       </div>
       <div className="buttons">
         <button className="button-nope">Nope</button>
