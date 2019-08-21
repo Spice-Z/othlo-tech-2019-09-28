5ab12505e47733081b87c52e14237294442d9778
Yugo Ogura <jigenbakdaaan@gmail.com>
2019-08-21 20:54:34 +0900

    cardコンポーネントでpropsを利用するように

--- src/card.js
+++ src/card.js
@@ -1,19 +1,12 @@
 import React from "react";
 
-export const Card = () => {
-  const [user, setUser] = React.useState({
-    name: "Yugo Spice Ogura",
-    age: 23,
-    image:
-      "https://images.unsplash.com/photo-1563170423-741125661ae8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80"
-  });
-
+export const Card = props => {
   return (
     <div className="card">
-      <img className="image" src={user.image} alt="human" />
+      <img className="image" src={props.image} alt="human" />
       <p>
-        {user.name}
-        <span>({user.age})</span>
+        {props.name}
+        <span>({props.age})</span>
       </p>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -5,9 +5,15 @@ import "./styles.css";
 import { Card } from "./card";
 
 function App() {
+  const [user, setUser] = React.useState({
+    name: "Yugo Spice Ogura",
+    age: 23,
+    image:
+      "https://images.unsplash.com/photo-1563170423-741125661ae8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80"
+  });
   return (
     <div className="App">
-      <Card />
+      <Card name={user.name} age={user.age} image={user.image} />
       <div className="buttons">
         <button className="button-nope">Nope</button>
         <button className="button-like">Like</button>
