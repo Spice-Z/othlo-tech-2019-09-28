528412c4cf0df2e1c8ab9a0c72cbb93744b424c5
Yugo Ogura <jigenbakdaaan@gmail.com>
2019-08-21 20:50:30 +0900

    cardをコンポーネント化

--- src/index.js
+++ src/index.js
@@ -2,24 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
+import { Card } from "./card";
 
 function App() {
-  const [user, setUser] = React.useState({
-    name: "Yugo Spice Ogura",
-    age: 23,
-    image:
-      "https://images.unsplash.com/photo-1563170423-741125661ae8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80"
-  });
-
   return (
     <div className="App">
-      <div className="card">
-        <img className="image" src={user.image} alt="human" />
-        <p>
-          {user.name}
-          <span>({user.age})</span>
-        </p>
-      </div>
+      <Card />
       <div className="buttons">
         <button className="button-nope">Nope</button>
         <button className="button-like">Like</button>
