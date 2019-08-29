f5d41d99b15062cfa26955ce9389a52acdd8a57c
Yugo Ogura <jigenbakdaaan@gmail.com>
2019-08-29 19:00:04 +0900

    カードの枚数を二枚以上に対応

--- src/index.js
+++ src/index.js
@@ -5,53 +5,83 @@ import "./styles.css";
 import { Card } from "./card";
 
 function App() {
-  const users = [
+  const [userList, setUserList] = React.useState([
     {
       name: "Yugo Spice Ogura",
       age: 23,
       image:
-        "https://images.unsplash.com/photo-1563170423-741125661ae8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80"
+        "https://images.unsplash.com/photo-1563170423-741125661ae8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80",
+      isSwiped: false
     },
     {
       name: "Yugo BOXPs Ogura",
       age: 26,
       image:
-        "https://images.unsplash.com/photo-1565385320868-2d6aa499b93d?ixlib=rb-1.2.1&auto=format&fit=crop&w=595&q=80"
+        "https://images.unsplash.com/photo-1565385320868-2d6aa499b93d?ixlib=rb-1.2.1&auto=format&fit=crop&w=595&q=80",
+      isSwiped: false
+    },
+    {
+      name: "Yugo Datch Ogura",
+      age: 26,
+      image:
+        "https://images.unsplash.com/photo-1532910404247-7ee9488d7292?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=582&q=80",
+      isSwiped: false
+    },
+    {
+      name: "Yugo OT Ogura",
+      age: 26,
+      image:
+        "https://images.unsplash.com/photo-1532910404247-7ee9488d7292?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=582&q=80",
+      isSwiped: false
+    },
+    {
+      name: "Yugo Melo Ogura",
+      age: 26,
+      image:
+        "https://images.unsplash.com/photo-1532910404247-7ee9488d7292?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=582&q=80",
+      isSwiped: false
     }
-  ];
+  ]);
 
-  const [currentPartner, setCurrentPartner] = React.useState(1);
-  const [nextPartner, setNextPartner] = React.useState(0);
-  const [isCurrentSwiped, setIsCurrentSwiped] = React.useState(false);
+  const [currentPartner, setCurrentPartner] = React.useState(userList[0]);
+  const [nextPartner, setNextPartner] = React.useState(userList[1]);
+
+  const clickLike = () => {
+    setCurrentPartner({
+      ...currentPartner,
+      isSwiped: true
+    });
+    console.log(userList);
+    setTimeout(() => {
+      setCurrentPartner(userList[1]);
+      setNextPartner(userList[2]);
+      setUserList(userList.slice(1));
+    }, 1000);
+  };
 
   return (
     <div className="App">
       <div className="card-container">
         {nextPartner !== undefined ? (
           <Card
-            key={users[nextPartner].name}
-            name={users[nextPartner].name}
-            age={users[nextPartner].age}
-            image={users[nextPartner].image}
-            isSwiped={false}
+            name={nextPartner.name}
+            age={nextPartner.age}
+            image={nextPartner.image}
+            isSwiped={nextPartner.isSwiped}
           />
         ) : null}
         {currentPartner !== undefined ? (
           <Card
-            key={users[currentPartner].name}
-            name={users[currentPartner].name}
-            age={users[currentPartner].age}
-            image={users[currentPartner].image}
-            isSwiped={isCurrentSwiped}
+            name={currentPartner.name}
+            age={currentPartner.age}
+            image={currentPartner.image}
+            isSwiped={currentPartner.isSwiped}
           />
         ) : null}
       </div>
       <div className="buttons">
         <button className="button-nope">Nope</button>
-        <button
-          className="button-like"
-          onClick={() => setIsCurrentSwiped(true)}
-        >
+        <button className="button-like" onClick={clickLike}>
           Like
         </button>
       </div>
